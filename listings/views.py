a42aea04327123417a222f0e6ca809c2c07b3a2e
Meendo <meenphilip@gmail.com>
2024-08-06 23:19:08 +0300

    Create listings & single listing views

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Listing
 
-# Create your views here.
+
+# Get all listings
+def listings_list(request):
+    listings = Listing.objects.all()
+    context = {"listings": listings}
+    return render(request, "listings/listings_list.html", context)
+
+
+def single_listing(request, id):
+    listing = get_object_or_404(Listing, id=id)
+    context = {"listing": listing}
+    return render(request, "listings/listing_details.html", context)
