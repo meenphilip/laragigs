750027b06244f321cbe397c9f78fcbbae601a4b0
Meendo <meenphilip@gmail.com>
2024-08-06 23:19:39 +0300

    Register listing model in the admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Listing
+
 
 # Register your models here.
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ["company_name", "job_title", "location"]
+    list_filter = ["created", "company_name", "job_title", "location"]
+    search_fields = ["company_name", "job_title", "location", "description"]
+    ordering = ["-created", "-company_name", "job_title"]
