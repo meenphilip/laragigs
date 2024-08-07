b4808302778ee6891932b18adfbc81967dae9958
Meendo <meenphilip@gmail.com>
2024-08-07 12:12:08 +0300

    Change views name

--- listings/views.py
+++ listings/views.py
@@ -3,7 +3,7 @@ from .models import Listing
 
 
 # Get all listings
-def listings_list(request):
+def listings(request):
     listings = Listing.objects.all()
     context = {"listings": listings}
     return render(request, "listings/listings_list.html", context)
@@ -11,5 +11,9 @@ def listings_list(request):
 
 def single_listing(request, id):
     listing = get_object_or_404(Listing, id=id)
+    
+    # option 2 to get single object
+    # listing = Listing.objects.get(id=id)
+    
     context = {"listing": listing}
     return render(request, "listings/listing_details.html", context)
