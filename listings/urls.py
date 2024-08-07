b28a89dce9245a297ce8e7056b05096ee80fb95d
Meendo <meenphilip@gmail.com>
2024-08-08 00:11:56 +0300

    Add create & delete listings urls

--- listings/urls.py
+++ listings/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.listings, name="listings"),
-    path("listing-detail/<int:id>", views.single_listing, name="single-listing"),
+    path("create_listings/", views.create_listing, name="create"),
+    path("listing-detail/<int:id>/", views.single_listing, name="single-listing"),
+    path("listing-update/<int:id>/", views.update_listing, name="listing_update"),
+    path("listing-delete/<int:id>/", views.delete_listing, name="delete"),
 ]
