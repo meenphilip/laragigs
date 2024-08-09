138e6f0ae9d94488ba628629622eaf149e960f2c
Meendo <meenphilip@gmail.com>
2024-08-09 21:43:37 +0300

    Override the form super method to attribute

--- listings/forms.py
+++ listings/forms.py
@@ -1,8 +1,8 @@
+# forms.py
 from django import forms
 from .models import Listing
 
 
-# listing form
 class ListingForm(forms.ModelForm):
     class Meta:
         model = Listing
@@ -15,3 +15,39 @@ class ListingForm(forms.ModelForm):
             "logo",
             "description",
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["company_name"].widget.attrs.update(
+            {"class": "border border-gray-200 rounded p-2 w-full"}
+        )
+        self.fields["job_title"].widget.attrs.update(
+            {
+                "class": "border border-gray-200 rounded p-2 w-full",
+                "placeholder": "Example: Senior Laravel Developer",
+            }
+        )
+        self.fields["location"].widget.attrs.update(
+            {
+                "class": "border border-gray-200 rounded p-2 w-full",
+                "placeholder": "Example: Remote, Boston MA, etc",
+            }
+        )
+        self.fields["email"].widget.attrs.update(
+            {"class": "border border-gray-200 rounded p-2 w-full"}
+        )
+        self.fields["tags"].widget.attrs.update(
+            {
+                "class": "border border-gray-200 rounded p-2 w-full",
+                "placeholder": "Example: Laravel, Backend, Postgres, etc",
+            }
+        )
+        self.fields["logo"].widget.attrs.update(
+            {"class": "border border-gray-200 rounded p-2 w-full"}
+        )
+        self.fields["description"].widget.attrs.update(
+            {
+                "class": "border border-gray-200 rounded p-2 w-full",
+                "placeholder": "Include tasks, requirements, salary, etc",
+            }
+        )
