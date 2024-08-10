376ddaee7e569936fdd0489a737c780cd43e544e
Meendo <meenphilip@gmail.com>
2024-08-10 22:12:37 +0300

    Add user login & user logout views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserLoginForm
 
-# Create your views here.
+
+# User login view
+def user_login(request):
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request, username=cd["username"], password=cd["password"]
+            )
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    next_url = request.POST.get("next") or request.GET.get("next")
+                    messages.success(request, "Login successful!")
+                    return redirect(next_url or "listings")
+                else:
+                    messages.error(request, "Disabled account.")
+            else:
+                messages.error(request, "Invalid credentials.")
+    else:
+        form = UserLoginForm()
+    return render(request, "users/login.html", {"form": form})
+
+
+# logout user
+def user_logout(request):
+    logout(request)
+    messages.success(request, "Login successful!")
+    return redirect("login")
