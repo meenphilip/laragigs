a3f758ade525dc0291a6c89f98168cfacd557eb8
Meendo <meenphilip@gmail.com>
2024-08-10 22:10:44 +0300

    Activate users urls

--- laragigs/urls.py
+++ laragigs/urls.py
@@ -23,7 +23,7 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("users/", include("users.urls")),
+    path("users/", include("users.urls")),
     path("", include("listings.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
