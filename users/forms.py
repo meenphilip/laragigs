3e7f115d1890c728595123faad2e35656846f663
Meendo <meenphilip@gmail.com>
2024-08-12 19:35:22 +0300

    Create user registration features

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Login Form
@@ -19,5 +20,39 @@ class UserLoginForm(forms.Form):
             {
                 "class": "border border-gray-200 rounded p-2 w-full",
                 "placeholder": "Enter your password",
+            }
+        )
+
+
+# User Registration Form
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget.attrs.update(
+            {
+                "class": "border border-gray-200 rounded p-2 w-full",
+                "placeholder": "Enter your username",
+            }
+        )
+        self.fields["email"].widget.attrs.update(
+            {
+                "class": "border border-gray-200 rounded p-2 w-full",
+                "placeholder": "Enter your email",
+            }
+        )
+        self.fields["password1"].widget.attrs.update(
+            {
+                "class": "border border-gray-200 rounded p-2 w-full",
+                "placeholder": "Enter your password",
+            }
+        )
+        self.fields["password2"].widget.attrs.update(
+            {
+                "class": "border border-gray-200 rounded p-2 w-full",
+                "placeholder": "Confirm your password",
             }
         )
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegistrationForm
 
 
 # User login view
@@ -33,3 +33,21 @@ def user_logout(request):
     logout(request)
     messages.success(request, "Login successful!")
     return redirect("login")
+
+
+# Register user
+def user_register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = form.save(commit=False)
+            # Set the chosen password using password1
+            new_user.set_password(form.cleaned_data["password1"])
+            # Save the User object
+            new_user.save()
+            messages.success(request, "Registration successful!")
+            return redirect("login")
+    else:
+        form = UserRegistrationForm()
+    return render(request, "users/register.html", {"form": form})
