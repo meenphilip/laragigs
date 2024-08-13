4b08a7878720b995457dca8f2b1bc8d15634f088
Meendo <meenphilip@gmail.com>
2024-08-13 22:57:08 +0300

    Switch to MYSQL DB

--- laragigs/settings.py
+++ laragigs/settings.py
@@ -107,26 +107,26 @@ WSGI_APPLICATION = "laragigs.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 # DB Configurations
-# MYSQL_USERNAME = os.environ.get("MYSQL_USER")
-# MYSQL_PASSWORD = os.environ.get("MYSQL_PASS")
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "laragigsDB",
-#         "USER": MYSQL_USERNAME,
-#         "PASSWORD": MYSQL_PASSWORD,
-#         "HOST": "127.0.0.1",
-#         "PORT": "3306",
-#     }
-# }
+MYSQL_USERNAME = os.environ.get("MYSQL_USER")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASS")
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "laragigsDB",
+        "USER": MYSQL_USERNAME,
+        "PASSWORD": MYSQL_PASSWORD,
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
